1207c6f20e51dc2540039f9ede5fe67db077e250
vangona <rlarhksrud14@gmail.com>
2023-01-05 00:37:47 +0900

    Env: prettier 설치

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import React from "react";
+import React from 'react';
 
 function App() {
-  return <div className="App">Hi!</div>;
+  return <div className='App'>Hi!</div>;
 }
 
 export default App;

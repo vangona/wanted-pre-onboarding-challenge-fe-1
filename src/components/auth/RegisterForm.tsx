f58b42a050e0cbc22c34358f253444481cb2180c
vangona <rlarhksrud14@gmail.com>
2023-01-05 22:07:09 +0900

    Feat: 회원가입 유효성 검사

--- src/components/auth/RegisterForm.tsx
+++ src/components/auth/RegisterForm.tsx
@@ -1,22 +1,73 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import * as Styled from '../../styles/auth/AuthForm.style';
+import checkIsValidEmail from '../../utils/checkIsValidEmail';
+import checkIsValidPassword from '../../utils/checkIsValidPassword';
 
 const RegisterForm = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [repassword, setRepassword] = useState('');
+  const [isValidForm, setIsValidForm] = useState(false);
+  const [error, setError] = useState('');
+
+  const handleChangeEmail: React.ChangeEventHandler = (event) => {
+    const target = event.target as HTMLInputElement;
+    setEmail(target.value);
+  };
+
+  const handleChangePassword: React.ChangeEventHandler = (event) => {
+    const target = event.target as HTMLInputElement;
+    setPassword(target.value);
+  };
+
+  const handleChangeRePassword: React.ChangeEventHandler = (event) => {
+    const target = event.target as HTMLInputElement;
+    setRepassword(target.value);
+  };
+
+  const handleSubmit: React.FormEventHandler = (event) => {
+    event.preventDefault();
+    if (password !== repassword) setError('비밀번호가 다릅니다.');
+  };
+
+  useEffect(() => {
+    if (checkIsValidEmail(email) && checkIsValidPassword(password)) {
+      setIsValidForm(true);
+    } else {
+      setIsValidForm(false);
+    }
+  }, [email, password]);
+
   return (
-    <Styled.Form>
+    <Styled.Form onSubmit={handleSubmit}>
       <Styled.Box>
         <Styled.Label>이메일 : </Styled.Label>
-        <Styled.EmailInput />
+        <Styled.EmailInput
+          type='text'
+          value={email}
+          onChange={handleChangeEmail}
+        />
       </Styled.Box>
       <Styled.Box>
         <Styled.Label>비밀번호 : </Styled.Label>
-        <Styled.PasswordInput />
+        <Styled.PasswordInput
+          type='password'
+          value={password}
+          onChange={handleChangePassword}
+        />
       </Styled.Box>
       <Styled.Box>
         <Styled.Label>비밀번호 확인 : </Styled.Label>
-        <Styled.PasswordInput />
+        <Styled.PasswordInput
+          type='password'
+          value={repassword}
+          onChange={handleChangeRePassword}
+        />
       </Styled.Box>
-      <Styled.SubmitButton>회원가입하기</Styled.SubmitButton>
+      <div>{error}</div>
+      <Styled.SubmitButton disabled={!isValidForm}>
+        회원가입하기
+      </Styled.SubmitButton>
     </Styled.Form>
   );
 };

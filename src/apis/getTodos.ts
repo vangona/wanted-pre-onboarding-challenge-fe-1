9df0808a4bce98510857569e7f80d56f7eebb7c0
vangona <rlarhksrud14@gmail.com>
2023-01-06 01:52:44 +0900

    Feat: getTodos api 수신 함수 구현

---
+++ src/apis/getTodos.ts
@@ -0,0 +1,35 @@
+import { API_HOST } from '@constants';
+
+type TodoData = {
+  title: string;
+  content: string;
+  id: string;
+  createdAt: string;
+  updatedAt: string;
+};
+
+interface GetTodosResponse {
+  data: TodoData[];
+}
+
+interface ErrorResponse {
+  details: string;
+}
+
+// token이 localStorage에 담겨있어서 null일 수 있기 때문에 외부에서 유효성을 체크해서 getTodos를 호출해준다.
+const getTodos = async (
+  token: string,
+): Promise<GetTodosResponse | ErrorResponse> => {
+  const url = new URL(API_HOST);
+  url.pathname = '/todos';
+
+  const response = await fetch(url, {
+    headers: {
+      Authorization: token,
+    },
+  });
+
+  return response.json();
+};
+
+export default getTodos;

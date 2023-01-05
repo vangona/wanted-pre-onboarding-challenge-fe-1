bf7853f414079493eec178da6dac1bb1b4dc3a53
vangona <rlarhksrud14@gmail.com>
2023-01-06 01:34:19 +0900

    Feat: Home 화면 출력 구현

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,6 +1,10 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import DetailSection from '@components/home/DetailSection';
+import HomeLayout from '@components/home/HomeLayout';
+import ListSection from '@components/home/ListSection';
 import checkIsValidToken from '@utils/checkIsValidToken';
+import type { Todo } from '#types/TodoTypes';
 
 interface HomeProps {
   userToken: string;
@@ -8,6 +12,19 @@ interface HomeProps {
 
 const Home = ({ userToken }: HomeProps) => {
   const navigate = useNavigate();
+  const [todo, setTodo] = useState<Todo | null>(null);
+  const [todos, setTodos] = useState<Todo[]>([
+    {
+      title: '꿀잠',
+      content: '안녕히 주무세용',
+      id: '1',
+    },
+    {
+      title: '뀨',
+      content: '다음 할 일',
+      id: '2',
+    },
+  ]);
 
   useEffect(() => {
     if (!checkIsValidToken(userToken)) {
@@ -15,7 +32,17 @@ const Home = ({ userToken }: HomeProps) => {
     }
   }, [userToken, navigate]);
 
-  return <div>Home, {userToken}</div>;
+  useEffect(() => {
+    setTodo(todos[0]);
+  }, [todos]);
+
+  return (
+    <HomeLayout>
+      {userToken}
+      <ListSection todos={todos} />
+      {todo && <DetailSection todo={todo} />}
+    </HomeLayout>
+  );
 };
 
 export default Home;

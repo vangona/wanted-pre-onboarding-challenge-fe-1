2d776c816617cdbbc7cab604fbe66a97cecdb9d0
vangona <rlarhksrud14@gmail.com>
2023-01-07 21:03:05 +0900

    Feat: AddTodoModal 레이아웃

--- src/components/common/AddTodoModal.tsx
+++ src/components/common/AddTodoModal.tsx
@@ -6,7 +6,23 @@ interface AddTodoModalProps {
 }
 
 const AddTodoModal = ({ onClose }: AddTodoModalProps) => {
-  return <Styled.Container onClick={onClose}>투두 추가 모달</Styled.Container>;
+  return (
+    <Styled.Container>
+      <Styled.Header>할 일 추가하기</Styled.Header>
+      <Styled.Form>
+        <Styled.Box>
+          <Styled.Label>할 일 제목 : </Styled.Label>
+          <Styled.TitleInput type='text' />
+        </Styled.Box>
+        <Styled.Box>
+          <Styled.Label>할 일 내용 : </Styled.Label>
+          <Styled.ContentInput type='text' />
+        </Styled.Box>
+        <Styled.SubmitButton>제출하기</Styled.SubmitButton>
+      </Styled.Form>
+      <Styled.CloseButton onClick={onClose}>x</Styled.CloseButton>
+    </Styled.Container>
+  );
 };
 
 export default AddTodoModal;
--- src/styles/common/AddTodoModal.style.tsx
+++ src/styles/common/AddTodoModal.style.tsx
@@ -5,8 +5,46 @@ export const Container = styled.div`
   position: absolute;
   top: 50%;
   left: 50%;
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  width: 300px;
+  height: 200px;
+  padding: 20px;
+  border-radius: 15px;
   transform: translate(-50%, -50%);
-  padding: 10px;
   z-index: ${Z_INDEX.MODAL};
   background-color: white;
+`;
+
+export const Header = styled.h1`
+  text-align: center;
+  font-size: 1.5rem;
+`;
+
+export const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+`;
+export const Box = styled.div`
+  display: flex;
+  gap: 10px;
+`;
+export const Label = styled.label``;
+export const TitleInput = styled.input``;
+export const ContentInput = styled.input``;
+export const SubmitButton = styled.button`
+  align-self: center;
+`;
+
+export const CloseButton = styled.button`
+  position: absolute;
+  right: -5px;
+  top: -5px;
+  border: none;
+  border-radius: 50%;
+  :hover {
+    cursor: pointer;
+  }
 `;

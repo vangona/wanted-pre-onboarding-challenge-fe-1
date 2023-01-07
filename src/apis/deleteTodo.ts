85462eb389636014678774e50cba083dbb14b077
vangona <rlarhksrud14@gmail.com>
2023-01-07 23:55:45 +0900

    Feat: deleteTodo api 구현

---
+++ src/apis/deleteTodo.ts
@@ -0,0 +1,29 @@
+import { API_HOST, METHOD } from '@constants';
+
+interface DeleteTodoResponse {
+  data: null;
+}
+
+interface ErrorResponse {
+  details: string;
+}
+
+// token이 localStorage에 담겨있어서 null일 수 있기 때문에 외부에서 유효성을 체크해서 호출해준다.
+const deleteTodo = async (
+  id: string,
+  token: string,
+): Promise<DeleteTodoResponse | ErrorResponse> => {
+  const url = new URL(API_HOST);
+  url.pathname = `/todos/${id}`;
+
+  const response = await fetch(url, {
+    method: METHOD.DELETE,
+    headers: {
+      Authorization: token,
+    },
+  });
+
+  return response.json();
+};
+
+export default deleteTodo;

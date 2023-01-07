3237f4ef16e13c73e8e38e35634c682fccf90983
vangona <rlarhksrud14@gmail.com>
2023-01-07 21:07:48 +0900

    Feat: AddTodoModal Form 구현

--- src/components/common/AddTodoModal.tsx
+++ src/components/common/AddTodoModal.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import * as Styled from '@styles/common/AddTodoModal.style';
 
 interface AddTodoModalProps {
@@ -6,17 +6,43 @@ interface AddTodoModalProps {
 }
 
 const AddTodoModal = ({ onClose }: AddTodoModalProps) => {
+  const [todoTitle, setTodoTitle] = useState<string>('');
+  const [todoContent, setTodoContent] = useState<string>('');
+
+  const handleChangeTodoTitle: React.ChangeEventHandler = (e) => {
+    const target = e.target as HTMLInputElement;
+    setTodoTitle(target.value);
+  };
+
+  const handleChangeTodoContent: React.ChangeEventHandler = (e) => {
+    const target = e.target as HTMLInputElement;
+    setTodoContent(target.value);
+  };
+
+  const handleSubmit: React.FormEventHandler = (e) => {
+    e.preventDefault();
+    console.log(todoContent, todoTitle);
+  };
+
   return (
     <Styled.Container>
       <Styled.Header>할 일 추가하기</Styled.Header>
-      <Styled.Form>
+      <Styled.Form onSubmit={handleSubmit}>
         <Styled.Box>
           <Styled.Label>할 일 제목 : </Styled.Label>
-          <Styled.TitleInput type='text' />
+          <Styled.TitleInput
+            type='text'
+            value={todoTitle}
+            onChange={handleChangeTodoTitle}
+          />
         </Styled.Box>
         <Styled.Box>
           <Styled.Label>할 일 내용 : </Styled.Label>
-          <Styled.ContentInput type='text' />
+          <Styled.ContentInput
+            type='text'
+            value={todoContent}
+            onChange={handleChangeTodoContent}
+          />
         </Styled.Box>
         <Styled.SubmitButton>제출하기</Styled.SubmitButton>
       </Styled.Form>

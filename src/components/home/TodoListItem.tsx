bea007f18cff124dc23f5984bde52a493f62757c
vangona <rlarhksrud14@gmail.com>
2023-01-11 22:13:58 +0900

    Refactor: deleteTodo api를 mutation으로 변경

--- src/components/home/TodoListItem.tsx
+++ src/components/home/TodoListItem.tsx
@@ -1,18 +1,16 @@
 import React from 'react';
+import useDeleteTodoMutation from '@hooks/mutations/useDeleteTodoMutation';
 import * as Styled from '@styles/home/TodoListItem.style';
 import type { Todo } from '#types/TodoTypes';
 
 interface TodoListItemProps {
   todo: Todo;
   handleClickTodo: (newTodo: Todo) => void;
-  handleClickDeleteTodo: (id: string) => void;
 }
 
-const TodoListItem = ({
-  todo,
-  handleClickTodo,
-  handleClickDeleteTodo,
-}: TodoListItemProps) => {
+const TodoListItem = ({ todo, handleClickTodo }: TodoListItemProps) => {
+  const { mutate: deleteTodo } = useDeleteTodoMutation();
+
   return (
     <Styled.ListItem key={todo.id}>
       <Styled.TodoTitle onClick={() => handleClickTodo(todo)}>
@@ -20,7 +18,7 @@ const TodoListItem = ({
       </Styled.TodoTitle>
       <Styled.DeleteButton
         onClick={() =>
-          window.confirm('삭제하시겠습니까?') && handleClickDeleteTodo(todo.id)
+          window.confirm('삭제하시겠습니까?') && deleteTodo({ todoId: todo.id })
         }
       >
         삭제하기
--- src/components/home/TodoListSection.tsx
+++ src/components/home/TodoListSection.tsx
@@ -1,9 +1,7 @@
 import React, { Suspense } from 'react';
-import apiDeleteTodo from '@apis/apiDeleteTodo';
 import TodoListItem from '@components/home/TodoListItem';
 import useGetTodosQuery from '@hooks/queries/useGetTodosQuery';
 import * as Styled from '@styles/home/TodoListSection.style';
-import getUserToken from '@utils/getUserToken';
 import type { Todo } from '#types/TodoTypes';
 
 interface TodoListSectionProps {
@@ -15,17 +13,8 @@ interface TodoListSectionProps {
 const TodoListSection = ({
   handleClickTodo,
   openModal,
-  handleDeleteTodoEffect,
 }: TodoListSectionProps) => {
-  const token = getUserToken();
   const { data: todos } = useGetTodosQuery();
-
-  const handleClickDeleteTodo = async (id: string) => {
-    await apiDeleteTodo(id, token);
-
-    alert('삭제되었습니다!');
-    handleDeleteTodoEffect(id);
-  };
 
   return (
     <Styled.Section>
@@ -37,7 +26,6 @@ const TodoListSection = ({
                 key={todo.id}
                 todo={todo}
                 handleClickTodo={handleClickTodo}
-                handleClickDeleteTodo={handleClickDeleteTodo}
               />
             ))}
             <Styled.AddButton onClick={openModal}>추가하기</Styled.AddButton>

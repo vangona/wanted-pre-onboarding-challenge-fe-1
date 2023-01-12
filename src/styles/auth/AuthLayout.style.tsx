e7a3671979956307d89437952366349d35682448
vangona <rlarhksrud14@gmail.com>
2023-01-12 23:34:27 +0900

    Feat: Auth route 레이아웃 구현

--- src/styles/auth/AuthLayout.style.tsx
+++ src/styles/auth/AuthLayout.style.tsx
@@ -1,3 +1,10 @@
 import styled from 'styled-components';
 
-export const Layout = styled.div``;
+export const Layout = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: var(--primary-green-color);
+`;

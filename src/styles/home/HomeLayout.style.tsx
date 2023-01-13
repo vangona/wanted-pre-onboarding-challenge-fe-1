3e1846c931be0be23fb5261e7dc1fa4ec2c4b33c
vangona <rlarhksrud14@gmail.com>
2023-01-13 15:55:47 +0900

    Feat: 배경 이미지 추가

--- src/styles/home/HomeLayout.style.tsx
+++ src/styles/home/HomeLayout.style.tsx
@@ -1,5 +1,11 @@
+import bgImg from '@assets/img/1.jpg';
 import styled from 'styled-components';
 
 export const Layout = styled.div`
+  width: 100%;
+  height: 100%;
   display: flex;
+  background-image: url(${bgImg});
+  background-size: cover;
+  background-position: center;
 `;

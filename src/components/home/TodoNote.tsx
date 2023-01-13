a02edd3797ecffb7f3efb4d6ac608f5fd329005e
vangona <rlarhksrud14@gmail.com>
2023-01-13 21:24:12 +0900

    Feat: TodoNote 떼기 버튼 구현

--- src/components/home/TodoNote.tsx
+++ src/components/home/TodoNote.tsx
@@ -1,20 +1,53 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useSearchParams } from 'react-router-dom';
 import TodoDetail from '@components/home/TodoDetail';
 import TodoEditForm from '@components/home/TodoEditForm';
 import useGetTodoByIdQuery from '@hooks/queries/useGetTodoByIdQuery';
 import * as Styled from '@styles/home/TodoNote.style';
+import { HOME_STYLE } from '@constants';
 
-const TodoNote = () => {
-  const [searchParams] = useSearchParams();
+interface TodoNoteProps {
+  noteOpacity: number;
+}
+
+const TodoNote = ({ noteOpacity }: TodoNoteProps) => {
+  const [searchParams, setSearchParams] = useSearchParams();
   const todoId = searchParams.get('todo');
   const { data: todoResponseBody } = useGetTodoByIdQuery(todoId || ''); // useQuery에서 enabled로 처리해줌.
   const [isEditMode, setIsEditMode] = useState<boolean>(false);
+  const [posLeft, setPosLeft] = useState<number>(0);
+  const [posTop, setPosTop] = useState<number>(0);
+
+  const getRandPos = () => {
+    const randLeft =
+      Math.random() *
+      (window.innerWidth -
+        parseInt(HOME_STYLE.LIST_SECTION_WIDTH.replace('px', '')) -
+        parseInt(HOME_STYLE.NOTE_WIDTH.replace('px', '')) -
+        HOME_STYLE.NOTE_POSITION_PADDING);
+    const randTop =
+      Math.random() *
+      (window.innerHeight -
+        parseInt(HOME_STYLE.NOTE_HEIGHT.replace('px', '')) -
+        HOME_STYLE.NOTE_POSITION_PADDING);
+    return [randLeft, randTop];
+  };
+
+  const handleDeleteNote = () => {
+    searchParams.delete('todo');
+    setSearchParams(searchParams);
+  };
+
+  useEffect(() => {
+    const [randLeft, randTop] = getRandPos();
+    setPosLeft(randLeft);
+    setPosTop(randTop);
+  }, []);
 
   if (!todoResponseBody) return <></>;
 
   return (
-    <Styled.Layout>
+    <Styled.Layout left={posLeft} top={posTop} defaultOpacity={noteOpacity}>
       {isEditMode ? (
         <TodoEditForm
           todo={todoResponseBody.data}
@@ -26,6 +59,7 @@ const TodoNote = () => {
           handleClickEdit={() => setIsEditMode(true)}
         />
       )}
+      <Styled.CloseButton onClick={handleDeleteNote}>X</Styled.CloseButton>
     </Styled.Layout>
   );
 };
--- src/styles/home/TodoNote.style.tsx
+++ src/styles/home/TodoNote.style.tsx
@@ -1,7 +1,14 @@
 import styled from 'styled-components';
 import { HOME_STYLE } from '@constants';
 
-export const Layout = styled.div`
+export const Layout = styled.div<{
+  left: number;
+  top: number;
+  defaultOpacity: number;
+}>`
+  position: absolute;
+  left: ${(props) => props.left}px;
+  top: ${(props) => props.top}px;
   display: flex;
   flex-direction: column;
   align-items: center;
@@ -9,7 +16,27 @@ export const Layout = styled.div`
   width: 320px;
   height: 240px;
   margin-left: ${HOME_STYLE.LIST_SECTION_WIDTH};
-  padding: ${HOME_STYLE.NOTE_PADDING};
+  padding: ${HOME_STYLE.NOTE_INNER_PADDING};
+  opacity: ${(props) => props.defaultOpacity};
   box-shadow: 10px 10px 10px rgba(0, 0, 0, 0.5);
   background-color: var(--beige-color);
+  transition: 0.3s all ease-in-out;
+  :hover {
+    opacity: 1;
+    transform: skewX(-1deg);
+  }
+`;
+
+export const CloseButton = styled.button`
+  position: absolute;
+  right: -5px;
+  top: -5px;
+  width: 20px;
+  height: 20px;
+  font-size: 10px;
+  border: none;
+  border-radius: 50%;
+  :hover {
+    cursor: pointer;
+  }
 `;

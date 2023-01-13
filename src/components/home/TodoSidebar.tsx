3ab9d8094edee163580750282aa2b8ae96118e4c
vangona <rlarhksrud14@gmail.com>
2023-01-13 20:09:43 +0900

    Feat: TodoListSection을 TodoSidebar로 변경

--- src/components/home/TodoSidebar.tsx
+++ src/components/home/TodoSidebar.tsx
@@ -1,17 +1,18 @@
 import React from 'react';
 import TodoListItem from '@components/home/TodoListItem';
 import useGetTodosQuery from '@hooks/queries/useGetTodosQuery';
-import * as Styled from '@styles/home/TodoListSection.style';
+import * as Styled from '@styles/home/TodoSidebar.style';
 
-interface TodoListSectionProps {
+interface TodoSidebarProps {
   openModal: () => void;
 }
 
-const TodoListSection = ({ openModal }: TodoListSectionProps) => {
+const TodoSidebar = ({ openModal }: TodoSidebarProps) => {
   const { data: todos } = useGetTodosQuery();
 
   return (
-    <Styled.Section>
+    <Styled.Layout>
+      <Styled.Title>오늘의 할 일 목록</Styled.Title>
       {todos && (
         <Styled.List>
           {todos.data.map((todo) => (
@@ -20,8 +21,8 @@ const TodoListSection = ({ openModal }: TodoListSectionProps) => {
           <Styled.AddButton onClick={openModal}>추가하기</Styled.AddButton>
         </Styled.List>
       )}
-    </Styled.Section>
+    </Styled.Layout>
   );
 };
 
-export default TodoListSection;
+export default TodoSidebar;
---
+++ src/styles/home/TodoSidebar.style.tsx
@@ -0,0 +1,41 @@
+import styled from 'styled-components';
+import { HOME_STYLE } from '@constants';
+
+export const Layout = styled.section`
+  position: absolute;
+  width: ${HOME_STYLE.LIST_SECTION_WIDTH};
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 40px;
+  padding: ${HOME_STYLE.SECTION_PADDING};
+  background-color: var(--primary-green-color);
+  color: var(--off-white-color);
+`;
+export const Title = styled.h1`
+  margin-top: 30px;
+  font-size: 1.5rem;
+  font-weight: bold;
+`;
+export const List = styled.ul`
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  line-height: 160%;
+`;
+
+export const AddButton = styled.button`
+  width: 250px;
+  height: 40px;
+  border-radius: 5px;
+  background-color: var(--brown-color);
+  color: var(--off-white-color);
+  :hover {
+    cursor: pointer;
+  }
+  :active {
+    transform: scale(0.98);
+  }
+`;

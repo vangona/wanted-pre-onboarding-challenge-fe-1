a5988429d66fd84fc619e9544567d26f20e79317
vangona <rlarhksrud14@gmail.com>
2023-01-23 18:26:48 +0900

    Fix: 긴 제목, 내용 작성시 에러 수정

--- src/styles/home/TodoDetail.style.tsx
+++ src/styles/home/TodoDetail.style.tsx
@@ -1,17 +1,26 @@
 import styled from 'styled-components';
 
 export const Form = styled.form`
+  width: 100%;
   display: flex;
   flex-direction: column;
   align-items: center;
   gap: 20px;
 `;
 export const Title = styled.h1`
+  width: 100%;
   margin-top: 20px;
   font-size: 1.2rem;
   font-weight: bold;
+  word-break: break-all;
+  white-space: normal;
 `;
-export const Content = styled.p``;
+export const Content = styled.p`
+  width: 100%;
+  height: 100%;
+  word-wrap: break-word;
+  overflow: auto;
+`;
 export const EditButton = styled.button`
   :hover {
     cursor: pointer;

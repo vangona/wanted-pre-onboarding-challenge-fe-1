b63459b6a652a53504f8da52acc56d0f523bc40c
vangona <rlarhksrud14@gmail.com>
2023-03-02 15:53:18 +0900

    Chore: Router path 수정

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -3,6 +3,7 @@ import { createRoutesFromElements, Route, RouterProvider } from 'react-router';
 import { createBrowserRouter } from 'react-router-dom';
 import ProtectedRoute from '@components/common/ProtectedRoute';
 import Auth from '@routes/Auth';
+import DailyscrumGroups from '@routes/DailyscrumGroups';
 import Home from '@routes/Home';
 import GlobalStyle from '@styles/Global.style';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
@@ -10,7 +11,6 @@ import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import checkIsValidToken from '@utils/checkIsValidToken';
 import getUserToken from '@utils/getUserToken';
 import { Reset } from 'styled-reset';
-import DailyscrumGroups from '@routes/DailyscrumGroups';
 
 const App = () => {
   const userToken = getUserToken();
@@ -41,7 +41,10 @@ const App = () => {
           }
           errorElement='에러가 발생했어용'
         />
-        <Route path='/dailyscrum/groups' element={<DailyscrumGroups />} />
+        <Route
+          path='/boogako/dailyscrum/groups'
+          element={<DailyscrumGroups />}
+        />
       </>,
     ),
   );
